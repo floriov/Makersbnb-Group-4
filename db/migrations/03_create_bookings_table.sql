58e0c47c4b262d175aa6f3af1d007e2cdb573fa0
Vanessa Florio <vanessaflorio1997@gmail.com>
2022-01-19 14:59:12 +0000

    merge changes Vanessa and Josh

--- db/migrations/03_create_bookings_table.sql
+++ db/migrations/03_create_bookings_table.sql
@@ -1,4 +1,3 @@
-CREATE TABLE bookings ( id SERIAL PRIMARY KEY, spaces_id int NOT NULL, host_id int NOT NULL, customer_id int NOT NULL, start_date date NOT NULL, end_date date NOT NULL, status status);
-
-Dropdown list for status column
 CREATE TYPE status AS ENUM ('requested', 'approved', 'declined', 'unavailable');
+
+CREATE TABLE bookings ( id SERIAL PRIMARY KEY, spaces_id int NOT NULL, host_id int NOT NULL, customer_id int NOT NULL, start_date date NOT NULL, end_date date NOT NULL, status status);
