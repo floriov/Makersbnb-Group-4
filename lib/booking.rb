cfe3c1fda3647d9e7609e12de19e73ef1c2043bb
Vanessa Florio <vanessaflorio1997@gmail.com>
2022-01-19 17:41:30 +0000

    added booking class, booking spec, all_booking made/received

--- lib/booking.rb
+++ lib/booking.rb
@@ -3,7 +3,19 @@ class Booking
   def self.add(space_id:, host_id:, customer_id:, start_date:, end_date:, status:)
     result = DatabaseConnection.query('INSERT INTO bookings (spaces_id, host_id, customer_id, start_date, end_date, status) 
     VALUES($1, $2, $3, $4, $5, $6) RETURNING spaces_id, host_id, customer_id, start_date, end_date, status;' [spaces_id, host_id, customer_id, start_date, end_date, status])
+   
+    Bookings.new(space_id: result[0]['space_id'], host_id: result[0]['host_id'], customer_id: result[0]['customer_id'], start_date: result[0]['start_date'], end_date: result[0]['end_date'])
+    
   end 
+
+  def self.all_booking_made(customer_id: )
+    result = DatabaseConnection.query("SELECT * FROM bookings WHERE customer_id = '#{customer_id}'")
+    
+  end
+
+  def self.all_booking_received(host_id:)
+    result = DatabaseConnection.query("SELECT * FROM bookings WHERE host_id = '#{host_id}'")
+  end
 
   def initialize(space_id:, host_id:, customer_id:, start_date:, end_date:, status:)
     @space_id = space_id
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -21,4 +21,46 @@ describe Booking do
       
     end 
   end 
+
+  describe '.all' do 
+    it 'shows all bookings' do
+      host = connection.exec_params("INSERT INTO users (id) VALUES ($1)", [1])
+      connection = PG.connect(dbname: 'makersbnb_test')
+      customer = connection.exec_params("INSERT INTO users (id) VALUES ($1)", [2])
+      space = Space.add(name: 'test name', description: 'test description', price: '22.22', user_id: 1, start_date: '2022-01-01', end_date: '2022-01-31')
+
+      booking = Bookings.add(space_id: space_id, host_id: 1, customer_id: 2, start_date: '2022-01-01', end_date: '2022-01-31', status: 'requested' )
+                Bookings.add(space_id: space_id, host_id: 1, customer_id: 2, start_date: '2022-01-01', end_date: '2022-01-31', status: 'requested' )
+
+        bookings = Booking.all
+      
+      expect(bookings.length).to eq 2
+      expect(bookings.first).to be_a Booking
+      expect(bookings.first.space_id).to eq booking.space_id
+      expect(bookings.first.host_id).to eq booking.host_id
+      expect(bookings.first.customer_id).to eq booking.customer_id
+      expect(bookings.first.start_date).to eq booking.start_date
+      expect(bookings.first.end_date).to eq booking.end_date
+      expect(bookings.first.status).to eq booking.status
+
+    end
+  end
+
+  describe '.all_booking_made' do
+    it 'shows the bookings requested by the customer' do
+      connection = PG.connect(dbname: 'makersbnb_test')
+      host = connection.exec_params("INSERT INTO users (id) VALUES ($1)", [1])
+      customer = connection.exec_params("INSERT INTO users (id) VALUES ($1)", [2])
+      space = Space.add(name: 'test name', description: 'test description', price: '22.22', user_id: 1, start_date: '2022-01-01', end_date: '2022-01-31')
+      booking = Bookings.add(space_id: space_id, host_id: 1, customer_id: 2, start_date: '2022-01-01', end_date: '2022-01-31', status: 'requested' )
+      Bookings.add(space_id: space_id, host_id: 1, customer_id: 2, start_date: '2022-01-01', end_date: '2022-01-31', status: 'requested' )
+    
+      bookings = Booking.all
+
+      expect(bookings.all_booking_made(customer_id: 2).length).to eq 2
+      
+    end
+  end
+  
 end 
+
