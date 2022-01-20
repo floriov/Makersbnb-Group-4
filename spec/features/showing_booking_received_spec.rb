39a3f9e7831fe19d5086dbca341d3ca4d66e459e
Vanessa Florio <vanessaflorio1997@gmail.com>
2022-01-20 16:02:25 +0000

    added showing booking received feature

---
+++ spec/features/showing_booking_received_spec.rb
@@ -0,0 +1,22 @@
+
+
+feature 'Showing booking received' do
+  scenario 'shows the booking received as a host' do
+    fill_in_add_space('Beautiful Relaxing Space', 'Less Beautiful, Less Relaxing Space', '142.00', '2022-01-02', '2022-04-02')
+    visit 'space/1'
+    fill_in 'start_date'
+    fill_in 'end_date'
+    click_button 'Submit'
+  
+    expect(page).to have_content('one request pending')
+
+  end
+end
+
+
+
+
+
+      
+
+    
