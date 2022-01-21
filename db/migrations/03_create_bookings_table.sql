6217f6f7c95f01e6c626089d9c49cfb804a40d35
Josh <joshlim092@gmail.com>
2022-01-21 12:11:04 +0000

    update 03 database migrations for bookings table

--- db/migrations/03_create_bookings_table.sql
+++ db/migrations/03_create_bookings_table.sql
@@ -1,2 +1,2 @@
 CREATE TYPE status AS ENUM ('requested', 'approved', 'declined', 'unavailable');
-CREATE TABLE bookings ( id SERIAL PRIMARY KEY, spaces_id INTEGER REFERENCES spaces (id) NOT NULL, host_id INTEGER REFERENCES users (id) NOT NULL, customer_id int NOT NULL, start_date date NOT NULL, end_date date NOT NULL, status status);
+CREATE TABLE bookings ( id SERIAL PRIMARY KEY, space_id INTEGER REFERENCES spaces (id) NOT NULL, host_id INTEGER REFERENCES users (id), customer_id int NOT NULL, start_date date NOT NULL, end_date date NOT NULL, status status);
